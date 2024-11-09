30d23000db8068386b89c789484ce1db82318729
HoundZel <wilsonpoon36@gmail.com>
2024-11-09 15:10:03 +0800

    ceasar cipher script

---
+++ calculators/ceasar_cipher.py
@@ -0,0 +1,16 @@
+original = str(input("Enter a code to be ciphered: "))
+key = int(input("Enter the key: "))
+
+convert_to = {}
+convert_fro = {}
+
+for i in range(97,122,1):
+    convert_to[chr(i)] = i-96
+    convert_fro[i-96] = chr(i)
+
+ciphered = ""
+for j in original:
+    code = (convert_to[j]+key)%26
+    ciphered += convert_fro[code]
+
+print("The ciphered code is: " + ciphered)
