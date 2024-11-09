becb95824a0d300f2019c53ee0729aa7e17e5890
HoundZel <wilsonpoon36@gmail.com>
2024-11-09 15:23:04 +0800

    bug patch

--- calculators/ceasar_cipher.py
+++ calculators/ceasar_cipher.py
@@ -4,13 +4,17 @@ key = int(input("Enter the key: "))
 convert_to = {}
 convert_fro = {}
 
-for i in range(97,122,1):
-    convert_to[chr(i)] = i-96
-    convert_fro[i-96] = chr(i)
+for i in range(97,123,1):
+    convert_to[chr(i)] = i-97
+    convert_fro[i-97] = chr(i)
+
+# print(convert_to)
+# print(convert_fro)
 
 ciphered = ""
 for j in original:
     code = (convert_to[j]+key)%26
+    # print(code)
     ciphered += convert_fro[code].upper()
 
 print("The ciphered code is: " + ciphered)
