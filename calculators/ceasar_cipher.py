81e5018a24da198f8585effd5d0bb9368327a8fd
Wilson <86096480+HoundZel@users.noreply.github.com>
2024-11-24 17:41:25 +0800

    cipher updated

--- calculators/ceasar_cipher.py
+++ calculators/ceasar_cipher.py
@@ -1,6 +1,5 @@
 original = str(input("Enter a code to be ciphered: "))
 key = int(input("Enter the key: "))
-rotate = int(input("Enter 0 to encrypt and 1 to decrypt: "))
 
 convert_to = {}
 convert_fro = {}
@@ -14,12 +13,13 @@ for i in range(97,123,1):
 # print(convert_fro)
 
 ciphered = ""
+diciphered = ""
 for j in original:
-    if rotate == 0:
-        code = (convert_to[j]+key)%26
-    else:
-        code = (convert_to[j]-key)%26
-    # print(code)
+    code = (convert_to[j]+key)%26
     ciphered += convert_fro[code].upper()
 
+    code = (convert_to[j]-key)%26
+    diciphered += convert_fro[code].upper()
+    
 print("The ciphered code is: " + ciphered)
+print("The diciphered code is: " + diciphered)
